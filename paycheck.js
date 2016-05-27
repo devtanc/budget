37b821dcad02dd72d0ef405fc155b7336472aaf0
devtanc <devtanc@users.noreply.github.com>
2016-05-26 20:56:10 -0600

    Calculate current paycheck

--- paycheck.js
+++ paycheck.js
@@ -1,25 +1,17 @@
 /* global module, require */
-var referencePaycheckDate = '2016-01-01';
-var format = 'YYYY-MM-DD';
-//var request = require('request');
-//var logger = require('./logger.js');
 var paychecks = require('./paychecks.js');
 var moment = require('moment');
-var search = require('binary-search');
 
-var paycheck = {};
+module.exports.getPaycheck = function(date) {
+	if(paychecks.dateFormatRegEx.test(date)){
+		var closestPaycheckDate = paychecks.dates.reduce(function(prev, current) {
+			if(current > date) { return prev; }
+			return current;
+		});
 
-paycheck.getCurrentPaycheck = function(currentDate) {
-  if(/\d{4}-\d{2}-\d{2}/.test(currentDate)){
-   return search(paychecks, currentDate, function(a, b) {
-      if(a < b) return -1;
-      if(a > b) return 1;
-      return 0;
-    });
-  } else { throw 'Invalid date format. Must follow "YYYY-MM-DD"'; }
+		return {
+			start: closestPaycheckDate,
+			end: moment(closestPaycheckDate).add(14, 'days').format(paychecks.format)
+		};
+	} else { throw 'Invalid date format. Must follow the format  m"' + paychecks.format + '"'; }
 };
-
-var returned = paycheck.getCurrentPaycheck(moment().format('YYYY-MM-DD'));
-console.log(paychecks[12], returned);
-
-module.exports = paycheck;
