614e645753d66a58a520d09dd2bc8f1bcc022319
devtanc <tanchristensen@vivintsolar.com>
2016-07-15 13:56:42 -0600

    Fixed auth issue

--- routes.js
+++ routes.js
@@ -102,9 +102,8 @@ module.exports = function(server) {
 
 	server.post('/api/pebbleEndpoint', function(req, res) {
 		var creds = auth(req);
-		if(!creds || creds.name !== 'pebble' || creds.pass !== process.env.PEBBLE_ENDPOINT_SECRET) {
+		if(!creds || creds.name !== 'pebble' || creds.pass !== process.env.PEBBLE_ENDPOINT_PASSWORD) {
 			logger.error(creds);
-			logger.error(JSON.stringify(creds));
 			res.status(500).json(creds).end();
 		} else {
 			res.status(201).json(req.body).end();
